228eb9852efc5d939dc92acf2f72b2fb928f70b2
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2024-12-16 14:45:20 +0200

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,14 @@
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import UserInfo from '../UserInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    <article
+      className={`TodoInfo TodoInfo--${todos.completed ? 'completed' : 'pending'}`}
+    >
+      <h2 className="TodoInfo__title">{todos.title}</h2>
+
+      <UserInfo />
+    </article>
+  </section>
+);
