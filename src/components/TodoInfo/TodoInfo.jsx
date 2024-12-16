9338af46447bdcbfdf217db93e753e2545d3d051
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2024-12-16 15:33:46 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,23 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+
+export const TodoInfo = ({ todos }) => (
+  <>
+    <article
+      className={`TodoInfo TodoInfo--${todos.completed ? 'completed' : 'pending'}`}
+    >
+      <h2 className="TodoInfo__title">{todos.title}</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">CSS</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+  </>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,11 @@
 // Add the required props
-import UserInfo from '../UserInfo';
+
+import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    <article
-      className={`TodoInfo TodoInfo--${todos.completed ? 'completed' : 'pending'}`}
-    >
-      <h2 className="TodoInfo__title">{todos.title}</h2>
-
-      <UserInfo />
-    </article>
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} />
+    ))}
   </section>
 );
